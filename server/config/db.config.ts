b16bf22f0314140cb9950ce79715258b4734e45a
Азат Юмагулов <jumagulov.azat@gmail.com>
2019-03-20 15:58:47 +0500

    Db connection singleton class

--- server/config/db.config.ts
+++ server/config/db.config.ts
@@ -1,9 +1,26 @@
 import * as mongoose from 'mongoose';
 
 
-export class DBConfig {
+/**
+ * Singleton db connect class
+ */
+class DBConfig {
 
-  static init(): void {
+  private static _instance: DBConfig;
+
+  private constructor() {
+  }
+
+  static get instance(): DBConfig {
+    if (!this._instance) {
+      this._instance = new DBConfig();
+    }
+
+    return this._instance;
+  }
+
+
+  init(): void {
     const mongodbURI: string = process.env.MONGODB_URI;
 
     mongoose.connect(mongodbURI, {
@@ -14,3 +31,7 @@ export class DBConfig {
       .catch(err => console.error(err));
   }
 }
+
+const DbInstance = DBConfig.instance;
+
+export { DbInstance as DBConfig };
